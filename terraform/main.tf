0dfc742ae320bdaf4043bd392dd85370f758a867
paulorcamargo <paulor1610@gmail.com>
2024-11-16 18:13:33 -0300

    init

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,8 +73,9 @@ resource "azurerm_linux_virtual_machine" "nginx" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_F2"
-  disable_password_authentication = true
+  disable_password_authentication = false
   admin_username      = "lnxuser"
+  admin_password = "!q2w3e4r5t"
   network_interface_ids = [
     azurerm_network_interface.nic01.id,
   ]
