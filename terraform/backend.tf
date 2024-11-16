65f90e23458b4ce03f42c9267e6dcccb1c0d9115
paulorcamargo <paulor1610@gmail.com>
2024-11-16 15:59:32 -0300

    init

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,7 +4,5 @@ terraform {
     storage_account_name = "terraformgitactions"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
-    tenant_id            = "${ secrets.AZURE_TENANT_ID }"
-    subscription_id      = "${ secrets.AZURE_SUBSCRIPTION_ID }"
   }
 }
