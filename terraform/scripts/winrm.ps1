d183710cefa0804f342f359b8a0f0f314488801d
paulorcamargo <paulor1610@gmail.com>
2024-11-17 14:54:40 -0300

    ansible

--- terraform/scripts/winrm.ps1
+++ terraform/scripts/winrm.ps1
@@ -1,17 +1,26 @@
-# Habilitar WinRM e configurar serviço
-Write-Output "Habilitando o WinRM e configurando o serviço..."
-winrm quickconfig -force
+Write-Host "Delete any existing WinRM listeners"
+winrm delete winrm/config/listener?Address=*+Transport=HTTP  2>$Null
+winrm delete winrm/config/listener?Address=*+Transport=HTTPS 2>$Null
 
-# Permitir autenticação básica
-Write-Output "Habilitando autenticação básica..."
-winrm set winrm/config/service/Auth '@{Basic="true"}'
-
-# Permitir comunicações não criptografadas
-Write-Output "Permitindo comunicações não criptografadas..."
+Write-Host "Create a new WinRM listener and configure"
+winrm create winrm/config/listener?Address=*+Transport=HTTP
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service '@{MaxConcurrentOperationsPerUser="12000"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
 
-# Configurar limite de memória para shells remotas
-Write-Output "Configurando o limite de memória para shells remotas..."
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}'
+Write-Host "Configure UAC to allow privilege elevation in remote shells"
+$Key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+$Setting = 'LocalAccountTokenFilterPolicy'
+Set-ItemProperty -Path $Key -Name $Setting -Value 1 -Force
 
-Write-Output "Configuração do WinRM concluída com sucesso!"
+Write-Host "turn off PowerShell execution policy restrictions"
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine -Force
+
+Write-Host "Configure and restart the WinRM Service; Enable the required firewall exception"
+Stop-Service -Name WinRM
+Set-Service -Name WinRM -StartupType Automatic
+netsh advfirewall firewall set rule name="Windows Remote Management (HTTP-In)" new action=allow localip=any remoteip=any
+Start-Service -Name WinRM
